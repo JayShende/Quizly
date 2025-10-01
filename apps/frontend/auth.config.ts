cd28dbb582568af06de809ddc530969791277638
JayShende <shendejay379@gmail.com>
2025-10-01 10:56:32 +0530

    chore(repo): setup turborepo with backend, prisma package, and NextAuth config >> >> - initialized Turborepo structure for monorepo management >> - added app/frontend with NextAuth configured via Boltgate for authentication >> - created shared packages/prisma package and configured it for frontend auth usage >> - added app/backend service and performed initial setup >>

---
+++ apps/frontend/auth.config.ts
@@ -0,0 +1,13 @@
+import GitHub from "next-auth/providers/github";
+import type { NextAuthConfig } from "next-auth";
+
+const authConfig: NextAuthConfig = {
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+  ],
+};
+
+export default authConfig;
---
+++ apps/frontend/route.ts
@@ -0,0 +1,30 @@
+/**
+ * Public routes (exact or pattern-based).
+ * - Strings = exact matches
+ * - Regex = dynamic patterns
+ */
+export const publicRoutes: (string | RegExp)[] = [
+  "/", // homepage
+  /^\/form\/[^/]+\/view$/, // matches /form/{anything}/view
+  /^\/form\/[^/]+\/edit$/, // matches /form/{anything}/view
+];
+
+/**
+ * An Array of Routes which are used for the authentication
+ * These Routes will redirect the loggedin users to /settings
+ * @type {string[]}
+ */
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+/**
+ * The Prefix for API authtentication routes
+ * Routes that start with this prefix are used for api authentication purposes
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The Default Redirect Path After Login
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/dashboard";
